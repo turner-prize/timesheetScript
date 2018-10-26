e71dcbac8d987472371332895e71693de8f34a6b
turner-prize <hiphoperation@hotmail.com>
2018-10-26 11:14:12 +0100

    added sqlite db and insert statement. sqlitedb may have dummy data in, will need to delete in future and add a line to write the schema if not existing instead

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import sqlite3
 import datetime
 
-ProjectNumbers = input("How many projects have you worked on today? (Include internal work like learning or MBD projects)")
+ProjectNumbers = int(input("How many projects have you worked on today? (Include internal work like learning or MBD projects)"))
 
-if type(ProjectNumbers) != int:
-    print ("need an integer lark")
-
-
-for i in ProjectNumbers:
-    Project = input("Enter Client/Project 1: ")
+for i in range(ProjectNumbers):
+    Project = input(f"Enter Client/Project {i}: ")
     TimeLength = input("How long did you spend?")
+    conn = sqlite3.connect('log.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO log VALUES (?,?,?)", (datetime.datetime.now(),Project,TimeLength))
+    conn.commit()
+    conn.close()
