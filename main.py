1af66f82cef40fd5bc6224a9b77ec4bc8b127e23
turner-prize <hiphoperation@hotmail.com>
2018-10-26 14:40:02 +0100

    added functionality to choose from an existing project or add new

--- main.py
+++ main.py
@@ -10,14 +10,36 @@ def CreateTable():
     conn.commit()
     conn.close()
 
+def GetProjects():
+    conn = sqlite3.connect('log.db')
+    c = conn.cursor()
+    c.execute("Select distinct project from log")
+    MyProjects = [i[0] for i in c.fetchall()]
+    MyProjects.sort()
+    conn.commit()
+    conn.close()
+    return MyProjects
+
+
 def run():
     if not os.path.isfile('log.db'):
         CreateTable()
-    ProjectNumbers = int(input("How many projects have you worked on today? (Include internal work like learning or MBD projects)"))
+    ProjectNumbers = int(input("How many projects have you worked on today?\n(Include internal work like learning or MBD projects) "))
 
     for i in range(ProjectNumbers):
-        Project = input(f"Enter Client/Project {i}: ")
-        TimeLength = input("How long did you spend?")
+        projectList = GetProjects()
+        print("Choose from previous projects, or enter new project \n")
+        for j,proj in enumerate(projectList):
+            print (f"{j}: {proj}")
+        Project = input(f"\nEnter number or new project: ")
+        try: 
+            Project = int(Project)
+            Project = projectList[Project]
+        except:
+            pass
+        TimeLength = input("How long did you spend? ")
+        if not i == (ProjectNumbers -1):
+            print("\n---Next Project---\n")
         conn = sqlite3.connect('log.db')
         c = conn.cursor()
         c.execute("INSERT INTO log VALUES (?,?,?)", (datetime.datetime.now(),Project,TimeLength))
